9457e64aa284db69af129c4381098a77e2c50928
Sasha <Sasha7b9@tut.by>
2017-12-16 22:39:57 +0300

    *

--- sources/libs/common/_VS/stdlib.h
+++ sources/libs/common/_VS/stdlib.h
@@ -1,8 +1,5 @@
 #pragma once
 
-void memcpy(void *, const void *, int);
-void memset(void *, int, int);
-int memcmp(void *, const void *, int);
 void *malloc(int);
 void free(void *);
 #define RAND_MAX 0x7fffffff
