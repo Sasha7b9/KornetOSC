6aaf8315e0836fc685ed9ea7ff5390cf8df7a203
Sasha7b9 <Sasha7b9@tut.by>
2018-02-28 13:43:19 +0300

    Panel Проект VS

--- sources/Panel/src/Hardware/Hardware.cpp
+++ sources/Panel/src/Hardware/Hardware.cpp
@@ -8,7 +8,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 Hardware hardware;
 
-static void ConfigSystemClock1(void);
+//static void ConfigSystemClock1(void);
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -95,23 +95,21 @@ void Hardware::ConfigSystemClock()
   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
-#pragma clang diagnostic ignored "-Wunused-function"
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
+/*
 static void ConfigSystemClock1(void)
 {
     RCC_OscInitTypeDef RCC_OscInitStruct;
     RCC_ClkInitTypeDef RCC_ClkInitStruct;
     RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
 
-    /**Configure the main internal regulator output voltage 
-    */
+    //Configure the main internal regulator output voltage 
     __HAL_RCC_PWR_CLK_ENABLE();
 
     __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
+    // Initializes the CPU, AHB and APB busses clocks 
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
     RCC_OscInitStruct.HSIState = RCC_HSI_ON;
     RCC_OscInitStruct.HSICalibrationValue = 16;
@@ -126,15 +124,13 @@ static void ConfigSystemClock1(void)
         ERROR_HANDLER();
     }
 
-    /**Activate the Over-Drive mode 
-    */
+    // Activate the Over-Drive mode 
     if (HAL_PWREx_EnableOverDrive() != HAL_OK)
     {
         ERROR_HANDLER();
     }
 
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
+    // Initializes the CPU, AHB and APB busses clocks 
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
         |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -148,26 +144,29 @@ static void ConfigSystemClock1(void)
     }
 
     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC;
-    PeriphClkInitStruct.PLLSAI.PLLSAIN = 192;        /* 50 ... 432 */
-    PeriphClkInitStruct.PLLSAI.PLLSAIR = 4;         /* 2 ... 7 */
+    PeriphClkInitStruct.PLLSAI.PLLSAIN = 192;       // 50 ... 432
+    PeriphClkInitStruct.PLLSAI.PLLSAIR = 4;         // 2 ... 7
     PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_2;
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
     {
         ERROR_HANDLER();
     }
 
-    /**Configure the Systick interrupt time 
-    */
+    // Configure the Systick interrupt time 
     HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-    /**Configure the Systick 
-    */
+    // Configure the Systick 
     HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-    /* SysTick_IRQn interrupt configuration */
+    // SysTick_IRQn interrupt configuration
     HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
+*/
+
+#ifdef MSVC
+#define __attribute(x)
+#endif
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 __attribute((noreturn)) void _Error_Handler(char *, int)
