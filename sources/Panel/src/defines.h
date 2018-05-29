537435f7eb15df5b09681e63705df5cde52da20e
Sasha7b9 <Sasha7b9@tut.by>
2018-05-29 14:28:52 +0300

    Panel Возврат на старую версию компилятора

--- sources/Panel/src/defines.h
+++ sources/Panel/src/defines.h
@@ -4,16 +4,6 @@
 #define __attribute(x)
 #endif
 
-#pragma clang diagnostic ignored "-Wglobal-constructors"
-#pragma clang diagnostic ignored "-Wpadded"
-#pragma clang diagnostic ignored "-Wold-style-cast"
-#pragma clang diagnostic ignored "-Wwritable-strings"
-#pragma clang diagnostic ignored "-Wchar-subscripts"
-#pragma clang diagnostic ignored "-Wmissing-field-initializers"
-#pragma clang diagnostic ignored "-Winvalid-source-encoding"
-#pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
-#pragma clang diagnostic ignored "-Wformat-nonliteral"
-
 #include <stm32f4xx.h>
 #include <stm32f4xx_hal.h>
 
