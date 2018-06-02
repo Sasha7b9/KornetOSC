8ad045957722a17980149d7d50053196b1499134
Sasha <Sasha7b9@tut.by>
2018-06-02 08:37:24 +0300

    OSC-Open Компилируется, но не работает

--- sources/Panel/src/defines.h
+++ sources/Panel/src/defines.h
@@ -4,8 +4,13 @@
 #define __attribute(x)
 #endif
 
+#ifdef OPEN
+#include <stm32f7xx.h>
+#include <stm32f7xx_hal.h>
+#else
 #include <stm32f4xx.h>
 #include <stm32f4xx_hal.h>
+#endif
 
 
 typedef const char *const   pString;
