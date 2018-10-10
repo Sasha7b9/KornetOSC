74d912bd0c52512d6b62e962e459f00624a516d1
Sasha7b9 <Sasha7b9@tut.by>
2018-10-10 16:46:57 +0300

    *

--- sources/Device/src/Hardware/FSMC.cpp
+++ sources/Device/src/Hardware/FSMC.cpp
@@ -367,10 +367,10 @@ void FSMC::SetOutData(uint8 data)
         CONFIGURE_TO_WRITE_PANEL;
     }
 
-    //                                          биты 0,1                            биты 2, 3
+    //                                                        биты 0,1                           биты 2, 3
     GPIOD->ODR = (GPIOD->ODR & 0x3ffc) + (uint16)(((int16)data & 0x03) << 14) + (((uint16)(data & 0x0c)) >> 2);
     
-    // Ѕиты 4,5,6,7
+    //                                                    Ѕиты 4,5,6,7
     GPIOE->ODR = (GPIOE->ODR & 0xf87f) + (uint16)(((int16)data & 0xf0) << 3);
 }
 
